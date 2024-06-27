d9d3118b02e623061ef55150b8040a3d53bf4775
sixteenis <psm0103907@naver.com>
2024-06-27 11:56:11 +0900

    (리팩) UserDefault 리팩토링 진행

---
+++ MeaningOutProject/Enum/UserDefaultsID.swift
@@ -0,0 +1,8 @@
+//
+//  UserDefaultKeyEnum.swift
+//  MeaningOutProject
+//
+//  Created by 박성민 on 6/27/24.
+//
+
+import Foundation
---
+++ MeaningOutProject/리팩토링/ProfileSetView.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileSetView.swift
+//  MeaningOutProject
+//
+//  Created by 박성민 on 6/25/24.
+//
+
+import UIKit
+
+import SnapKit
+
+class ProfileSetView: BaseView {
+    
+    override func setUpHierarchy() {
+        <#code#>
+    }
+    override func setUpLayout() {
+        <#code#>
+    }
+}
